f4607a9016d77a7dbf780dc335fb4c7f4ae8307d
adham.muhammad <adhamfarid1110@gmail.com>
2025-09-14 13:42:58 +0700

    feat[homepage]: add Adham Farid name on gray color

--- src/pages/HomePage/Hero.tsx
+++ src/pages/HomePage/Hero.tsx
@@ -1,4 +1,7 @@
 import { forwardRef } from "react";
+import TypingText from "../../components/ui/shadcn-io/typing-text";
+
+import "./Hero.scss";
 
 interface HeroProps {
   // you can still have custom props here if needed
@@ -6,8 +9,13 @@ interface HeroProps {
 
 const Hero = forwardRef<HTMLDivElement, HeroProps>((_, ref) => {
   return (
-    <div ref={ref} className="h-screen bg-red-100">
-      Hero
+    <div ref={ref} className="flex flex-col justify-center">
+      <h1 className="text-[26vw] leading-[0.9] font-black text-gray-200">
+        ADHAM
+      </h1>
+      <h1 className="text-[31vw] leading-[0.9] font-black text-gray-200 tracking-wider">
+        FARID
+      </h1>
     </div>
   );
 });
